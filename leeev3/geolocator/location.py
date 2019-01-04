3b7115f82cf007b1016003955f0ced60e62bbf3e
Edwin Lee <leeed2001@gmail.com>
2019-01-04 08:16:03 -0600

    Pump up vector class, flake8

--- leeev3/geolocator/location.py
+++ leeev3/geolocator/location.py
@@ -15,12 +15,12 @@ class Vector(object):
     def __init__(self, x: float, y: float, angle: float = None, x2: float = None, y2: float = None):
         """
         There are two possible ways to create a vector here, from a point and an angle, or from two points
-        In the first case, pass in x, y, and angle.  In the second case, pass in x, y, x2, and y2.  No other 
-        combinations are valid.        
-        
+        In the first case, pass in x, y, and angle.  In the second case, pass in x, y, x2, and y2.  No other
+        combinations are valid.
+
         :param x: Measured from the left, in meters
         :param y: Measured from the top, in meters
-        :param angle: Measured counter clockwise from East, so East is 0, and North is 90
+        :param angle: Measured counter clockwise from East, in radians, so East is 0, and North is pi/2
         :param x2: Measured from the left, in meters
         :param y2: Measured from the top, in meters
         """
@@ -28,9 +28,13 @@ class Vector(object):
         self.y = y
 
         if x2 is not None and y2 is not None and angle is None:
-            pass  # ok, store it from that data
+            self.angle = math.atan2(y2-y, x2-x)  # TODO: might assume the wrong DeltaY direction?
         elif angle is not None and x2 is None and y2 is None:
-            pass  # ok, store it from that data
+            self.angle = angle
         else:
             raise Exception('Bad input argument combination in Vector constructor')
-        self.angle = angle
+
+    def get_end_xy_at_radius(self, radius):
+        x2 = self.x + radius * math.cos(self.angle)
+        y2 = self.y + radius * math.sin(self.angle)
+        return [x2, y2]
